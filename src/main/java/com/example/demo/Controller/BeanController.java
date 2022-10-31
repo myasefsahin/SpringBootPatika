a05d9971022a966d5f15475f69c04052f23ef9c7
myasefsahin <mustafasahin5529@gmail.com>
2022-10-31 19:23:18 +0300

    Bean Controller

---
+++ src/main/java/com/example/demo/Controller/BeanController.java
@@ -0,0 +1,21 @@
+package com.example.demo.Controller;
+
+import com.example.demo.bean.BeanConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BeanController {
+
+    @Autowired
+    BeanConfig beanConfig;
+
+    //http://localhost8080/bean/beanDto
+    @GetMapping("bean/beanDto")
+    @ResponseBody
+    public String getBeanDto(){
+     return beanConfig.beanDto()+" ";
+    }
+}
