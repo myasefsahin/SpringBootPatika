fbb389463ca586cd72711e7d97454ec178fc35b9
myasefsahin <mustafasahin5529@gmail.com>
2022-10-31 18:39:08 +0300

    application

--- src/main/java/com/example/demo/SpringBootPatikaApplication.java
+++ src/main/java/com/example/demo/SpringBootPatikaApplication.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootPatikaApplication {
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
+public class SpringBootPatikaApplication{
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPatikaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPatikaApplication.class, args);
+    }
 
 }
