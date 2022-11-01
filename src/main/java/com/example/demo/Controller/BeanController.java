0822b26d9bd89b4e0446194d8af456850e94d0e5
myasefsahin <mustafasahin5529@gmail.com>
2022-11-01 17:46:47 +0300

    Bean Controller

--- src/main/java/com/example/demo/Controller/BeanController.java
+++ src/main/java/com/example/demo/Controller/BeanController.java
@@ -1,10 +1,13 @@
 package com.example.demo.Controller;
 
 import com.example.demo.bean.BeanConfig;
+import jdk.swing.interop.SwingInterOpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.logging.Logger;
 
 @Controller
 public class BeanController {
@@ -12,10 +15,13 @@ public class BeanController {
     @Autowired
     BeanConfig beanConfig;
 
+
     //http://localhost8080/bean/beanDto
     @GetMapping("bean/beanDto")
     @ResponseBody
     public String getBeanDto(){
      return beanConfig.beanDto()+" ";
     }
+
+
 }
