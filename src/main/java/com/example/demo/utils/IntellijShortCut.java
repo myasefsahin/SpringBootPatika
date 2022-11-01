58b06771085a1dce44d824b46147441da67a0d4e
myasefsahin <mustafasahin5529@gmail.com>
2022-11-01 17:46:16 +0300

    İntellij ShortCut Settings

--- src/main/java/com/example/demo/utils/IntellijShortCut.java
+++ src/main/java/com/example/demo/utils/IntellijShortCut.java
@@ -1,10 +1,11 @@
 package com.example.demo.utils;
 
 public class IntellijShortCut {
-//    Setting ==> ctrl+alt+s
-//    Alt+Ins ==> constructor, toString ,getter setter
-//    Ctrl+/ => single comment
-//    Ctrl+shift+/ => multiple comment
-    //Ctrl+alt+L=formmater
+   // Setting ==> ctrl+alt+s
+    //Alt+Ins ==> constructor, toString ,getter setter
+   // Ctrl+/ => single comment
+   // Ctrl+shift+/ => multiple comment
+   // Ctrl+alt+L=formmater
+
 
 }
