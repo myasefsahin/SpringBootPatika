918e8ca666dfd90def76ba499eeb15c8d278c8b8
myasefsahin <mustafasahin5529@gmail.com>
2022-11-01 17:16:15 +0300

    thymeleaf html,css,model

---
+++ src/main/java/com/example/demo/Controller/ThymeleafController.java
@@ -0,0 +1,33 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ThymeleafController {
+
+
+    //http://localhost8080/thymeleaf1
+    //@ResponseBody
+    @GetMapping("/thymeleaf1")
+    public String getThymeleafController1() {
+        return "thymeleaf1";
+    }
+
+    @GetMapping("/thymeleaf2")
+    public String getThymeleaf2Model(Model model) {
+        model.addAttribute("key_model1", "Ben model 1 den geldim");
+        model.addAttribute("key_model2", "Ben model 2 den geldim");
+        return "thymeleaf1";
+    }
+
+    @GetMapping("/thymeleaf3")
+    public String getThymeleaf3Model(Model model) {
+        model.addAttribute("key_model1", "Ben model 1 den geldim");
+        model.addAttribute("key_model2", "Ben model 2 den geldim");
+        return "thymeleaf_file/thymeleaf3";
+
+    }
+}
