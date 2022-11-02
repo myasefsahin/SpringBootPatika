21f24ac62d1c69f17e0a6998e25e58a818351b26
myasefsahin <mustafasahin5529@gmail.com>
2022-11-02 14:36:09 +0300

    start application

--- src/main/java/com/example/demo/SpringBootPatikaApplication.java
+++ src/main/java/com/example/demo/SpringBootPatikaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
 )
-public class SpringBootPatikaApplication{
+public class SpringBootPatikaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootPatikaApplication.class, args);
