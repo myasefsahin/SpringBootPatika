72ca9bcff2e6e00a54781b7ffcbb7ecaa7e886d0
Mustafa Yasef Şahin <mustafasahin5529@gmail.com>
2022-11-03 15:03:38 +0300

    EmployeeDto

---
+++ src/main/java/com/example/demo/dto/EmployeeEntity.java
@@ -0,0 +1,22 @@
+package com.example.demo.dto;
+
+//DTO : Data Transfer Object
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Log4j2
+@Builder
+public class EmployeeEntity {
+    private Long id;
+    private String firstname;
+    private String Lastname;
+    private String emailId;
+
+}
